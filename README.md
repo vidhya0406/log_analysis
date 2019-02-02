8f68a53541c5964c1178548f051ced7a51346bda
Vidhya Ranganathan <vidhya.ranganathan@auth0.com>
2019-02-02 00:56:03 -0800

    Log analysis project

---
+++ README.md
@@ -0,0 +1,56 @@
+### Log Analysis
+
+##Project Overview
+The database contains newspaper articles, as well as the web server log for the site. The log has a database row for each time a reader loaded a web page. Using that information, your code will answer questions about the site's user activity.
+
+The program you write in this project will run from the command line. It won't take any input from the user. Instead, it will connect to that database, use SQL queries to analyze the log data, and print out the answers to some questions.
+
+## Steps to setup the project
+
+
+#### Installing dependencies
+
+- Install [Vagrant](https://www.vagrantup.com/)
+- Install [VirtualBox](https://www.virtualbox.org/)
+- Download the vagrant setup files from [Udacity's Github](https://github.com/udacity/fullstack-nanodegree-vm)
+
+These files configure the virtual machine and install all the tools needed to run this project.
+
+- Download the database setup: [data](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip)
+- Unzip the same to get the newsdata.sql file.
+- Put the newsdata.sql file into the vagrant directory. This will automatically be available inside the virtual machine.
+- Download this project in the vagrant directory
+
+#### In VM:
+
+- Navigate to the directory that has the `vagrant` file.
+- ``` vagrant up ``` to build the VM. (Required only during the first run)
+- ``` vagrant ssh ``` to connect.
+- ``` cd /vagrant/log_analysis ```
+- ``` psql -d news -f newsdata.sql ``` to load the data.
+- ``` python log_analysis.py```
+
+
+
+## Views created to make query easier
+```
+CREATE VIEW requests AS
+SELECT time::date AS day, count(*)
+            FROM log
+            GROUP BY time::date
+            ORDER BY time::date;
+
+CREATE VIEW err_rate AS
+SELECT requests.day,
+        errors.count::float / req.count::float * 100
+        AS percentage
+    FROM requests, errors
+    WHERE requests.day = errors.day;
+
+CREATE VIEW err_rate AS
+SELECT requests.day,
+        errors.count::float / requests.count::float * 100
+        AS percentage
+    FROM requests, errors
+    WHERE requests.day = errors.day;
+```
---
+++ log_analysis.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+import psycopg2
+
+
+def connect_to_database():
+    """
+    Connect to a database
+
+    Return:
+        Cursor for db operations
+    """
+    try:
+        database = psycopg2.connect("dbname=news")
+        cursor = database.cursor()
+    except psycopg2.Error as err:
+        print("Failed to connect to the PSQL database: \n", err)
+        return None
+    else:
+        return cursor
+
+
+def popular_three_articles(db_cursor):
+    """
+     Finds the most popular three artciles.
+
+        :param db_cursor: db cursor to perform query fetch.
+    """
+
+    query = """
+        SELECT authors.name, COUNT(*) AS num
+        FROM authors
+        JOIN articles
+        ON authors.id = articles.author
+        JOIN log
+        ON log.path like concat('/article/%', articles.slug)
+        GROUP BY authors.name
+        ORDER BY num DESC
+        LIMIT 3;
+    """
+    db_cursor.execute(query)
+    results = db_cursor.fetchall()
+
+    print('\n What are the most popular three articles of all time? \n')
+    print('===========================================================')
+
+    for result in results:
+        print('\n"{title}" - {count} views \n'
+              .format(title=result[0], count=result[1]))
+
+    return
+
+
+def most_popular_authors(db_cursor):
+    """
+        Finds the most popular authors.
+        :param db_cursor: Cursor to perform query fetch db operation
+    """
+    query = """
+            SELECT authors.name,
+                   count(*) AS num
+            FROM   log,
+                   articles,
+                   authors
+            WHERE  log.path = '/article/' || articles.slug
+            AND articles.author = authors.id
+            GROUP BY authors.name
+            ORDER BY num DESC;
+        """
+    db_cursor.execute(query)
+    results = db_cursor.fetchall()
+    print('\n Who are the most popular article authors of all time? \n')
+    print('===========================================================')
+
+    for result in results:
+        print('\n {author} - {count} views \n '
+              .format(author=result[0], count=result[1]))
+    return
+
+
+def one_percent_errors(db_cursor):
+    """
+        Finds the day where error is more than 1%
+        :param db_cursor: Cursor to perform query fetch db operation
+
+        Note:
+        Query in this function depends on views that were created in the db.
+        Refer README for more details.
+
+    """
+    query = """
+            SELECT * FROM err_rate WHERE percentage > 1;
+            """
+    db_cursor.execute(query)
+    results = db_cursor.fetchall()
+
+    print('\n On which days did more than 1% of requests lead to errors? \n')
+    print('=================================================================')
+
+    for result in results:
+        print('\n {date:%B %d, %Y} - {rate:.1f}% errors\n'.format(
+            date=result[0],
+            rate=result[1]))
+
+    return
+
+
+if __name__ == "__main__":
+    CURSOR = connect_to_database()
+    if CURSOR:
+        popular_three_articles(CURSOR)
+        most_popular_authors(CURSOR)
+        one_percent_errors(CURSOR)
+        CURSOR.close()
